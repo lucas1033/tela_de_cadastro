021a1f8eb3e0d2c77d0af7dfe85860528bbec3fd
Aluno <Aluno@Digital.net>
2019-06-12 16:20:05 -0300

    cadastro

---
+++ meu/routes/usuarios.js
@@ -0,0 +1,100 @@
+// não mexa nestas 3 linhas!
+var express = require('express');
+var router = express.Router();
+var banco = require('../app-banco');
+// não mexa nessas 3 linhas!
+
+router.post('/entrar', function (req, res, next) {
+
+  banco.conectar().then(() => {
+    console.log(`Chegou p/ login: ${JSON.stringify(req.body)}`);
+    var login = req.body.login;
+    var senha = req.body.senha; // depois de .body, use o nome (name) do campo em seu formulário de login  
+    if (login == undefined || senha == undefined) {
+      throw new Error(`Dados de login não chegaram completos: ${login} / ${senha}`);
+    }
+    return banco.sql.query(`select * from login where email='${login}' and senha='${senha}'`);
+  }).then(consulta => {
+
+    console.log(`Usuários encontrados: ${JSON.stringify(consulta.recordset)}`);
+
+    if (consulta.recordset.length == 1) {
+      res.send(consulta.recordset[0]);
+    } else {
+      res.sendStatus(404);
+    }
+
+  }).catch(err => {
+
+    var erro = `Erro no login: ${err}`;
+    console.error(erro);
+    res.status(500).send(erro);
+
+  }).finally(() => {
+    banco.sql.close();
+  });
+
+});
+
+
+router.post('/cadastrar', function (req, res, next) {
+
+  var nome;
+  var vingador;
+  var login;
+  var senha;
+  var cadastro_valido = false;
+
+  banco.conectar().then(() => {
+    console.log(`Chegou p/ cadastro: ${JSON.stringify(req.body)}`);
+    nome = req.body.nome; // depois de .body, use o nome (name) do campo em seu formulário de login
+    vingador = req.body.vingador;
+    login = req.body.login; // depois de .body, use o nome (name) do campo em seu formulário de login
+    senha = req.body.senha; // depois de .body, use o nome (name) do campo em seu formulário de login
+    if (nome == undefined || vingador == undefined || login == undefined || senha == undefined) {
+      // coloque a frase de erro que quiser aqui. Ela vai aparecer no formulário de cadastro
+      throw new Error(`Dados de cadastro não chegaram completos: ${nome} / ${vingador} / ${login} / ${senha}`);
+    }
+    return banco.sql.query(`select count(*) as contagem from login where email = '${login}'`);
+  }).then(consulta => {
+
+    if (consulta.recordset[0].contagem >= 1) {
+      res.status(400).send(`Já existe usuário com o login "${login}"`);
+      return;
+    } else {
+      console.log('válido!');
+      cadastro_valido = true;
+    } 
+
+  }).catch(err => {
+
+    var erro = `Erro no cadastro: ${err}`;
+    console.error(erro);
+    res.status(500).send(erro);
+
+  }).finally(() => {
+    if (cadastro_valido) {
+
+      banco.sql.query(`insert into login (nome,vingador, email, senha) values ('${nome}','${vingador}','${login}','${senha}')`).then(function () {
+        console.log(`Cadastro criado com sucesso!`);
+        res.sendStatus(201);
+        // status 201 significa que algo foi criado no back-end, 
+        // no caso, um registro de usuário ;)		
+      }).catch(err => {
+
+        var erro = `Erro no cadastro: ${err}`;
+        console.error(erro);
+        res.status(500).send(erro);
+
+      }).finally(() => {
+        banco.sql.close();
+      });
+    }
+  });
+
+
+});
+
+
+// não mexa nesta linha!
+module.exports = router;
